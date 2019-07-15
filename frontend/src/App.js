d24c39507210f77b8434bb1dd6732df3b881faf2
unknown <fdsa4321jkl@yahoo.com>
2019-07-14 22:04:51 -0600

    routing partial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
