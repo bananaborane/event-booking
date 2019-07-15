3cced1da7c647cabfeb926ce8815fc7d5481d1e7
unknown <fdsa4321jkl@yahoo.com>
2019-07-15 10:10:46 -0600

    main nav component added

---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const mainNavigation = props => (
+    <header>
+        <div className='main-navigation__logo'>
+            <h1>EasyEvent</h1>
+        </div>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default mainNavigation;
