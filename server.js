dabc159518e4ea3f4655b9c84bcf5033969370fd
unknown <fdsa4321jkl@yahoo.com>
2019-07-16 21:24:21 -0600

    backend api hit

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ const events = []
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth)
 
 
@@ -26,8 +36,8 @@ app.use(
 );
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-1gex1.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(()=>{
-    app.listen(3000);
-    console.log(`Server be listenin to port 3000`)
+    app.listen(8000);
+    console.log(`Server be listenin to port 8000`)
 }).catch((err)=>{
     console.log(err)
 })
