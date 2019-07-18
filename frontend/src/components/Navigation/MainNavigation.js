862f1f160a2b94a7f12734e3c6486d9e70decb41
unknown <fdsa4321jkl@yahoo.com>
2019-07-18 15:10:55 -0600

    button styling

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -20,9 +20,16 @@ const mainNavigation = props => (
           <li>
             <NavLink to="/events">Events</NavLink>
           </li>
-          {context.token && <li>
-            <NavLink to="/bookings">Bookings</NavLink>
-          </li>}
+          {context.token && (
+          <React.Fragment>
+            <li>
+              <NavLink to="/bookings">Bookings</NavLink>
+            </li>
+            <li>
+              <button>Logout</button>
+            </li>
+            </React.Fragment>
+          )}
         </ul>
       </nav>
     </header>)
